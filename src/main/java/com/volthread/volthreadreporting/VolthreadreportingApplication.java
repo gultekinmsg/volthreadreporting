024524f2deda4e8746469df0e765fc79a2d7605a
Mami <gultekinmsg@gmail.com>
2021-05-29 16:33:57 +0300

    Initial commit

---
+++ src/main/java/com/volthread/volthreadreporting/VolthreadreportingApplication.java
@@ -0,0 +1,13 @@
+package com.volthread.volthreadreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolthreadreportingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolthreadreportingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volthread/volthreadreporting/VolthreadreportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volthread.volthreadreporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolthreadreportingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
